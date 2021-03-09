6bcb5b8aeade97616b2410f8cff01297b446ea34
GerardCod <gerardo.aguilar@alumno.uttehuacan.edu.mx>
2021-03-09 15:11:07 -0600

    [MODIFY] Routing implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,20 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Nav from './secure/components/Nav';
+import SideNav from './secure/components/SideNav';
+import Dashboard from './secure/Dashboard';
+import Users from './secure/Users';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Login from './public/Login';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Route path='/' component={Login} />
+      <Route path='/users' component={Users} />
+      <Route path='/dashboard' component={Dashboard} />
+    </BrowserRouter>
   );
 }
 
