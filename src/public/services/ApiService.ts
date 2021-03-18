db50ab7126dcb6a8d79b81d51ec563723f57c152
GerardCod <gerardo.aguilar@alumno.uttehuacan.edu.mx>
2021-03-18 17:20:06 -0600

    [MODIFY] Implementing axios in the project

--- src/public/services/ApiService.ts
+++ src/public/services/ApiService.ts
@@ -1,29 +1,8 @@
-import { environtment } from "../../global";
-import { CreateUser } from "../models/CreateUser";
-import { LoginUser } from "../models/LoginUser";
+import axios from 'axios';
 
-
-export const signupUser = async (data: CreateUser): Promise<any> => {
-    const responseRaw = await fetch(`${environtment.url}${environtment.auth.signup}`, generateRequestInit(data, 'POST'));
-    const responseJSON = await responseRaw.json();
-    return responseJSON;
-}
-
-export const loginUser = async (data: LoginUser): Promise<any> => {
-    const responseRaw = await fetch(`${environtment.url}${environtment.auth.login}`, generateRequestInit(data, 'POST'));
-    const responseJSON = await responseRaw.json();
-    return responseJSON;
-}
-
-export const generateRequestInit = (data: any, method: string): RequestInit => {
-    const headers: HeadersInit = {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-    };
-    const opts: RequestInit = {
-        method,
-        body: JSON.stringify(data),
-        headers
-    };
-    return opts;
-}
+export default axios.create({
+    baseURL: 'http://127.0.0.1:8000/api',
+    headers: {
+        Authorization: `Bearer ${localStorage.getItem('token')}`,
+    }
+})
