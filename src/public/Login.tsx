869eac94a7dc110626498a7a57eaa99decbd18e7
GerardCod <gerardo.aguilar@alumno.uttehuacan.edu.mx>
2021-03-18 16:33:27 -0600

    [MODIFY] Finishing the login

--- src/public/Login.tsx
+++ src/public/Login.tsx
@@ -1,15 +1,32 @@
-import React, { Fragment } from 'react';
+import React, { ChangeEvent, FormEvent, useState } from 'react';
+import { LoginUser } from './models/LoginUser';
 import './Public.css';
+import { loginUser } from './services/ApiService';
 
 const Login = () => {
+    const [data, setData] = useState(new LoginUser('',''));
+
+    const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+        setData({
+            ...data,
+            [event.target.name]: event.target.value
+        })
+    }
+
+    const submitData = async (event: FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        const response = await loginUser(data);
+        console.log(response)
+    }
+
     return (
         <div className="form-signin">
-            <form>
+            <form onSubmit={submitData}>
                 <h1 className="h3 mb-3 fw-normal">Please sign in</h1>
                 <label htmlFor="inputEmail" className="visually-hidden">Email address</label>
-                <input type="email" id="inputEmail" className="form-control" placeholder="Email address" required autoFocus />
+                <input type="email" id="inputEmail" className="form-control" placeholder="Email address" name="email" onChange={handleChange} required autoFocus />
                 <label htmlFor="inputPassword" className="visually-hidden">Password</label>
-                <input type="password" id="inputPassword" className="form-control" placeholder="Password" required />
+                <input type="password" id="inputPassword" className="form-control" placeholder="Password" name="password" onChange={handleChange} required />
                 <div className="checkbox mb-3">
                     <label>
                         <input type="checkbox" value="remember-me" /> Remember me
--- src/public/services/ApiService.ts
+++ src/public/services/ApiService.ts
@@ -1,9 +1,16 @@
 import { environtment } from "../../global";
 import { CreateUser } from "../models/CreateUser";
+import { LoginUser } from "../models/LoginUser";
 
 
 export const signupUser = async (data: CreateUser): Promise<any> => {
     const responseRaw = await fetch(`${environtment.url}${environtment.auth.signup}`, generateRequestInit(data, 'POST'));
+    const responseJSON = await responseRaw.json();
+    return responseJSON;
+}
+
+export const loginUser = async (data: LoginUser): Promise<any> => {
+    const responseRaw = await fetch(`${environtment.url}${environtment.auth.login}`, generateRequestInit(data, 'POST'));
     const responseJSON = await responseRaw.json();
     return responseJSON;
 }
